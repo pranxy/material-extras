58f95ce2592c8c4155920d7bb30597c26f686f11
Pedro Fernandes <pedroice@gmail.com>
2021-03-23 17:58:39 +0100

    chore: fix images and routing

--- projects/docs/src/app/shared/shared.module.ts
+++ projects/docs/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../material-modules';
 import { NavbarComponent } from './navbar/navbar.component';
 
 const components = [NavbarComponent];
 
 @NgModule({
-    imports: [CommonModule, MaterialModule],
+    imports: [CommonModule, MaterialModule, RouterModule],
     exports: [CommonModule, MaterialModule, ...components],
     declarations: [...components],
 })
